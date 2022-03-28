136b9a26d637e4c1048763065bd58988597461f5
AsktroNaut <votuan2791999@gmail.com>
2022-03-28 09:19:16 +0700

    a

--- js/store.js
+++ js/store.js
@@ -1,11 +1,20 @@
 
 let currentProducts = productsData.topProducts.tshirt
 
-console.log(currentProducts.forEach(function(currentProduct) {
-    currentProduct.id
-}));
 
 
+
+let newList = [];
+productsData.topProducts.hoodie.forEach(i=>{
+    let temporaryId = i.id.toString().slice(0,4);
+    if(!newList.find(i=>i.id==temporaryId)){
+        
+        let tempProduct = JSON.parse(JSON.stringify(i));
+        tempProduct.id = temporaryId;
+        newList.push(tempProduct);
+    }
+})
+// console.log(newList);
 
 
 
